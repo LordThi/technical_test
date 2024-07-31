fc529cefdbc7d0b86396b3cfb4f54ce3dc232354
Thibaut Livigni <tlivigni@axe-e-sante.com>
2024-07-31 15:30:40 +0200

    :hammer: [Front-App] Add <Footer/>+<Instruction/>+ login logic+ handle login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Login from "./components/Login.jsx";
+import Footer from "./components/Footer.jsx";
+import Instructions from "./components/Instructions.jsx";
 
 function App() {
     const [data, setData] = useState(null);
+    const [candidat, setCandidat] = useState({});
+    const [isLogged, setLogged] = useState(false);
 
     useEffect(() => {
         fetch('/api/data')
@@ -11,9 +15,22 @@ function App() {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
+    const company = data ? data.company : null;
+
+    const handleCandidat = (candidat) => {
+        console.log('le candidat est: ', candidat);
+        setCandidat(candidat);
+        if (candidat.firstname.length && candidat.lastname && candidat.mail)
+            setLogged(true)
+    };
+
     return (
         <div>
-            <Login/>
+            {console.log(data)}
+            {console.log(candidat)}
+            {!isLogged && <Login handleCandidat={handleCandidat}/>}
+            {isLogged && <Instructions/>}
+            <Footer company={company}/>
         </div>
     );
 }
