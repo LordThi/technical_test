4b85be65b68993095882ea2652b8c3ee3ba469b2
Thibaut Livigni <tlivigni@axe-e-sante.com>
2024-07-31 15:28:28 +0200

    :hammer: [Front-Login] Add validation logic + speach + animation submitbutton

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,62 +1,82 @@
-import React, { useState } from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Form, Button, Container, Row, Col, Fade} from 'react-bootstrap';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
 
-
-const Login = () => {
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [email, setEmail] = useState('');
+const Login = ({handleCandidat}) => {
+    const [candidat, setCandidat] = useState({
+        firstname: '',
+        lastname: '',
+        mail: ''
+    })
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('Prénom:', firstname);
-        console.log('Nom:', lastname);
-        console.log('Email:', email);
+        handleCandidat(candidat);
+    };
+
+    const isFormComplete = () => {
+        return candidat.firstname !== '' && candidat.lastname !== '' && candidat.mail !== '';
     };
 
     return (
-        <Container className="mt-5">
+        <Container className="mt-5 center">
+            {console.log(candidat.firstname)}
             <h2 className="mb-4">Login</h2>
+            <p>
+                Bienvenue sur notre plateforme de tests technique. Votre profil a su attirer notre attention et nous
+                souhaiterions vous connaitre un peu plus.<br/>
+                Afin de vous offrir une expérience adaptée, veuillez renseigner ces champs avant de passer à la suite.
+            </p>
             <Form onSubmit={handleSubmit}>
                 <Row className="mb-3">
                     <Col>
                         <Form.Group controlId="firstname">
-                            <Form.Label>Prénom</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="Prénom"
-                                value={firstname}
-                                onChange={(e) => setFirstname(e.target.value)}
+                                value={candidat.firstname}
+                                onChange={(e) => setCandidat({...candidat, firstname: e.target.value})}
                                 required
                             />
                         </Form.Group>
                     </Col>
                     <Col>
                         <Form.Group controlId="lastname">
-                            <Form.Label>Nom</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="Nom"
-                                value={lastname}
-                                onChange={(e) => setLastname(e.target.value)}
+                                value={candidat.lastname}
+                                onChange={(e) => setCandidat({...candidat, lastname: e.target.value})}
                                 required
                             />
                         </Form.Group>
                     </Col>
                 </Row>
-                <Form.Group className="mb-3" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Votre email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Soumettre <i className="fas fa-check"></i>
-                </Button>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                value={candidat.mail}
+                                onChange={(e) => setCandidat({...candidat, mail: e.target.value})}
+                                required
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col className="d-flex justify-content-end">
+                        <Fade in={isFormComplete()}>
+                            <div>
+                                <Button variant="primary" type="submit">
+                                    Let's go!&nbsp;
+                                    <FontAwesomeIcon icon={faChevronRight}/>
+                                </Button>
+                            </div>
+                        </Fade>
+
+                    </Col>
+                </Row>
             </Form>
         </Container>
     );
