2200ca4fd1c62da48d4a53b4081e94e91b6c91f4
Thibaut Livigni <tlivigni@axe-e-sante.com>
2024-08-08 18:01:17 +0200

    :hammer: [Front - API - Candidat] Update api routes and methods

--- client/src/services/apiCandidat.js
+++ client/src/services/apiCandidat.js
@@ -1,9 +1,40 @@
+// src/services/apiCandidat.js
 import axios from 'axios';
 
+const fetchCandidats = async () => {
+    return axios.get('/api/candidats');
+};
+
+const fetchTypesPoste = async () => {
+    return axios.get('/api/types_poste');
+};
+
+const fetchNiveaux = async () => {
+    return axios.get('/api/niveaux');
+};
+
+const fetchQuizzes = async () => {
+    return axios.get('/api/quizzes');
+};
+
 const submitCandidat = async (data) => {
-    return axios.post('/api/candidat', data);
+    return axios.post('/api/candidats', data);
+};
+
+const updateCandidat = async (id, data) => {
+    return axios.put(`/api/candidats/${id}`, data);
+};
+
+const deleteCandidat = async (id) => {
+    return axios.delete(`/api/candidats/${id}`);
 };
 
 export default {
-    submitCandidat
+    fetchCandidats,
+    fetchTypesPoste,
+    fetchNiveaux,
+    fetchQuizzes,
+    submitCandidat,
+    updateCandidat,
+    deleteCandidat
 };
